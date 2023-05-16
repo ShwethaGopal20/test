118bc3731c1ba558ace7000d9a5cc900075a2db8
ShwethaGopal20 <124663728+ShwethaGopal20@users.noreply.github.com>
2023-05-16 12:23:58 +0530

    Update saucen.cy.js

--- cypress/e2e/saucen.cy.js
+++ cypress/e2e/saucen.cy.js
@@ -1,19 +1,12 @@
 /// <reference types="cypress"/>
 
 describe('sauce demo website', () => {
-
     it('negative cases', () => {
-    
       cy.visit('https://www.saucedemo.com/');
-      
       cy.title().should('include', 'Swag Labs');
-
       cy.get('[data-test="username"]').type('shwetha');
-
       cy.get('[data-test="password"]').type('shwetha@123');
-
       cy.get('[data-test="login-button"]').click();
-
       cy.get('#error-message').should('contain.text', 'Epic sadface: Username and password do not match any user in this service');
     })
   })
